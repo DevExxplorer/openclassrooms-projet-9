dbc6c207c826fa9ed58c06ccaa6fe0ecac167141
devexplorer <contact@devexplorer.fr>
2025-01-15 20:33:07 -0400

    add placeholder input text

--- litrevu/app/forms.py
+++ litrevu/app/forms.py
@@ -17,7 +17,11 @@ class SubscribeForm(UserCreationForm):
         self.fields['password2'].help_text = ""
 
 class LoginForm(forms.Form):
-    username = forms.CharField(label="name", max_length=100)
+    username = forms.CharField(
+        label="name",
+        max_length=100,
+        widget=forms.TextInput(attrs={'placeholder': 'Nom d\'utilisateur'})
+    )
     password = forms.CharField(
         widget=forms.PasswordInput(attrs={'placeholder': 'Mot de passe'}),
     )
