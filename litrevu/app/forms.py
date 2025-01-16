e113eb5428b6ea4e77c4be0c6282bec3f43c68b4
devexplorer <contact@devexplorer.fr>
2025-01-15 20:49:49 -0400

    transfer to new package

--- litrevu/app/forms.py
+++ litrevu/app/forms.py
@@ -1,20 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from .models import User ,Ticket ,Review
-
-class SubscribeForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ['username', 'password1', 'password2']
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs.update({'placeholder': 'Nom d\'utilisateur'})
-        self.fields['password1'].widget.attrs.update({'placeholder': 'Mot de passe'})
-        self.fields['password2'].widget.attrs.update({'placeholder': 'Confirmez le mot de passe'})
-        self.fields['username'].help_text = ""
-        self.fields['password1'].help_text = ""
-        self.fields['password2'].help_text = ""
+from .models import Ticket ,Review
 
 class LoginForm(forms.Form):
     username = forms.CharField(
