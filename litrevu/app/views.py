e0e0376e31481ba2ae32f6ebedc7ca0fb5960b70
devexplorer <contact@devexplorer.fr>
2025-02-17 10:07:21 -0400

    fix menu view and active logout link

--- litrevu/app/views.py
+++ litrevu/app/views.py
@@ -3,25 +3,28 @@ from django.contrib.auth import authenticate, login
 from .forms import LoginForm
 
 def home(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-
-        if form.is_valid():
-            username = request.POST['username']
-            password = request.POST['password']
-
-            user = authenticate(request, username=username, password=password)
-
-            if user is not None:
-                login(request, user)
-                return redirect("flux")
-            else:
-                message = 'Ce compte n\'existe pas'
-        else:
-            message = 'Identifiant ou Mot de passe invalide'
+    if request.user.is_authenticated:
+        return redirect("flux")
     else:
-        form = LoginForm()
-        message = ''
+        if request.method == 'POST':
+            form = LoginForm(request.POST)
+
+            if form.is_valid():
+                username = request.POST['username']
+                password = request.POST['password']
+
+                user = authenticate(request, username=username, password=password)
+
+                if user is not None:
+                    login(request, user)
+                    return redirect("flux")
+                else:
+                    message = 'Ce compte n\'existe pas'
+            else:
+                message = 'Identifiant ou Mot de passe invalide'
+        else:
+            form = LoginForm()
+            message = ''
 
     return render(
         request,
--- litrevu/litrevu/urls.py
+++ litrevu/litrevu/urls.py
@@ -5,14 +5,21 @@ from authentificate import views as auth_views
 from book_reviews import views as book_reviews_views
 from followers import views as followers_views
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+
+def custom_logout(request):
+    logout(request)  # Déconnecte l'utilisateur
+    return redirect("home")  # Redirige vers la page d'accueil
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", app_views.home, name="home"),
     path("inscription/", auth_views.subscribe),
+    path("flux/", login_required(book_reviews_views.flux_page), name="flux"),
     path("creation-ticket/", login_required(book_reviews_views.new_ticket_page), name="creation-ticket"),
     path("creation-critique/", login_required(book_reviews_views.new_review_page), name="creation-critique"),
     path("creation-critique/<int:id_ticket>/", login_required(book_reviews_views.new_review_page), name="creation-critique-answer"),
-    path("flux/", login_required(book_reviews_views.flux_page), name="flux"),
     path("abonnements/", login_required(followers_views.page_views)),
+    path("logout/", custom_logout, name="logout"),
 ]
